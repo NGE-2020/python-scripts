fa70b81efbba10f78e4cd5ff12ffd272db3fefd1
ricardoguque <ricardoguque@gmail.com>
2020-02-25 22:57:33 -0500

    more structured

--- virl-preparation/virl-from-scratch-v1.py
+++ virl-preparation/virl-from-scratch-v1.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 
 import threading
 import getpass
@@ -11,11 +10,9 @@ import json
 import requests
 from pprint import pprint
 
-def devices_data():
-    """
-    Takes device data from VIRL server using the REST API
-    Finally returns a list of lists
-    """
+initial_time = time.time()
+
+def virl_device_data():
     response = requests.get('http://10.10.20.160:19399/roster/rest/', auth=('guest', 'guest'))
     uglyjson = response.json()
     dic_list = [v for k,v in uglyjson.items()]
@@ -33,7 +30,6 @@ def test_ssh_con(address, nodename, username, password):
         ssh_client = paramiko.SSHClient()
         ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh_client.connect(hostname=address, username=username, password=password)
-        time.sleep(1)
         print("\nSSH is properly configured in",nodename)
         return('true')
 
@@ -41,8 +37,7 @@ def test_ssh_con(address, nodename, username, password):
         pass
 
     except:
-        time.sleep(1)
-        print("\nSSH is not configured in",nodename)
+        print("\nSSH is not properly configured in",nodename)
         return('false')
 
     finally:
@@ -53,27 +48,28 @@ def sshconnect(mgmt_ip, port, nodename, nodetype, username, password):
     ssh_attempt = test_ssh_con(mgmt_ip, nodename, username, password)
 
     if ssh_attempt == 'false':
-        time.sleep(1)
         print('\n Configuring SSH on',nodename, 'with ip', mgmt_ip)
 
         if nodetype == 'IOS XRv':
             print('Type not suported yet')
+
         elif nodetype == 'CSR1000v':
             tn = telnetlib.Telnet(mgmt_ip)
-            tn.write(b" \n")
-            tn.read_until(b">")
-            tn.write(b" enable\n")
+            tn.read_until(b"Username: ")
+            tn.write(username.encode('ascii') + b"\n")
             tn.read_until(b"Password: ")
             tn.write(password.encode('ascii') + b"\n")
-            tn.read_until(b"Username: ")
-            # tn.write(username.encode('ascii') + b"\n")
-            # tn.read_until(b"Password: ")
-            # tn.write(password.encode('ascii') + b"\n")
-            tn.write(b" conf t\n crypto key generate rsa modulus 1024\n")
-            tn.read_until(b"#")
-            tn.write(b" ip domain-name ngn.com \n ip ssh version 2\n aaa new-model\n exit\n wr\n")
-            tn.read_until(b"#")
-            tn.write(b" exit\n exit\n")
+            tn.write(b"   conf t\n   crypto key generate rsa modulus 1024\n")
+            time.sleep(1)
+            tn.write(b"   ip domain-name ngn.com \n   ip ssh version 2\n   aaa new-model\n   exit\n   wr\n")
+            time.sleep(1)
+            tn.write(b"   netconf-yang \n  yes\n ")
+            time.sleep(1)
+            tn.write(b"   netconf-yang feature candidate-datastore\n ")
+            time.sleep(1)
+            tn.write(b"   ip http secure-server\n   restconf\n")
+            time.sleep(1)
+            tn.write(b"   exit\n   exit\n")
             tn.close()
             print("SSH configuration done in \n",nodename)
 
@@ -98,29 +94,19 @@ def prepare_threaths(device_info, password, username, function):
     for tr in threaths:
         tr.join()
 
-def testssh_threaths(device_info, password, username, function):
-    threaths = []
+if __name__ == '__main__':
+
+    user = input("Enter your username: ")
+    pswd = getpass.getpass()
+
+    device_info = virl_device_data()
+
+    prepare_threaths(device_info, pswd, user, sshconnect)
+
+    print("\n Let's make a final test to confirm\n")
 
     for device_list in device_info:
         mgmt_ip = device_list[3]
         nodename = device_list[0]
-        try:
-            th = threading.Thread(target=function, args=(mgmt_ip, nodename, username, password, ))
-            th.start()
+        test_ssh_con(mgmt_ip, nodename, user, pswd)
 
-        finally:
-            threaths.append(th)
-
-    for tr in threaths:
-        tr.join()
-
-user = input("Enter your username: ")
-pswd = getpass.getpass()
-
-device_info = virl_device_data()
-
-prepare_threaths(device_info, pswd, user, sshconnect)
-
-print("\n Let's make a final test to confirm\n")
-
-testssh_threaths(device_info, pswd, user, test_ssh_con)
